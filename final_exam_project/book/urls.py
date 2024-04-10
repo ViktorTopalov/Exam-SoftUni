e28482a76d12d8b4d720d829f4a7234449847add
ViktorTopalov <139811576+ViktorTopalov@users.noreply.github.com>
2024-04-10 11:50:32 +0300

    Update urls.py

--- final_exam_project/book/urls.py
+++ final_exam_project/book/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 
 from final_exam_project.book.views import CreateBookView, delete_book, edit_book, book_details, exploring_books, \
-    like_book, add_comment, liked_books, uploaded_books
+    like_book, add_comment, liked_books, uploaded_books, save_book, saved_books
 
 urlpatterns = (
     path('uploaded_books/',uploaded_books,name='uploaded_books'),
     path('liked_books/',liked_books, name='liked_books'),
+    path('saved_books/', saved_books, name='saved_books'),
+
     path('like/<int:book_id>/', like_book, name='like_book'),
+    path('save/<int:book_id>/', save_book, name='save_book'),
     path('comment/<int:book_id>/', add_comment, name='add_comment'),
     path('create/',CreateBookView.as_view(),name='create_book'),
     path('exploring-books/',exploring_books,name='exploring_books'),
